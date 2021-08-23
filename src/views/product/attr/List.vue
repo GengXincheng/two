29720539ee52469f79acfd63cc15e4025a3486a7
Your Name <you@example.com>
2021-08-23 16:21:57 +0800

    写完数据修改之后,保存按钮逻辑写完之后,重新刷新页面显示503

--- src/views/product/attr/List.vue
+++ src/views/product/attr/List.vue
@@ -33,7 +33,7 @@
                 title="修改"
                 size="mini"
                 icon="el-icon-edit"
-                 @click="shoeUpdataDiv(row)"
+                @click="shoeUpdataDiv(row)"
               ></HintButton>
               <HintButton
                 type="danger"
@@ -74,28 +74,49 @@
           <el-table-column prop="prop" label="属性值名称" width="width">
             <template slot-scope="{ row, $index }">
               <el-input
+                v-if="row.isEdit"
+                :ref="$index"
                 v-model="row.valueName"
                 placeholder="请输入属性值名称"
+                size="mini"
+                @blur="toLook(row)"
+                @keyup.enter.native="toLook(row)"
               ></el-input>
+              <span
+                v-else
+                @click="toEdit(row, $index)"
+                style="display:'block;height:'100%';width:'100%'"
+                >{{ row.valueName }}</span
+              >
             </template>
           </el-table-column>
           <el-table-column prop="prop" label="操作" width="width">
-            <template slot-scope="{row,$index}">
-             <HintButton type='warning' size='mine' icon='el-icon-delete'  title="删除" ></HintButton>
+            <template slot-scope="{ row, $index }">
+              <el-popconfirm
+                title="这是一段内容确定删除吗？"
+                @onConfirm="attrForm.attrValueList.splice($index, 1)"
+              >
+                <template #reference>
+                  <HintButton
+                    type="warning"
+                    size="mine"
+                    icon="el-icon-delete"
+                    title="删除"
+                  >
+                  </HintButton>
+                </template>
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
-        <el-button type="primary">保存</el-button>
+        <el-button type="primary" >保存</el-button>
         <el-button type="primary" @click="isShoeList = true">取消</el-button>
       </div>
     </el-card>
   </div>
 </template>
 <script>
-// import { Level } from "node_modules/_chalk@2.4.2@chalk/types";
-  import cloneDeep from 'lodash/cloneDeep'
-  // import cloneDeep from ''
-  // import cloneDeep from ''
+import cloneDeep from "lodash/cloneDeep";
 export default {
   name: "attr",
   data() {
@@ -106,10 +127,10 @@ export default {
       attrList: [],
       isShoeList: true,
       attrForm: {
-        attrName:'',
-        attrValueList:[],
-        categoryId:0,
-        categoryLevel:3,
+        attrName: "",
+        attrValueList: [],
+        categoryId: 0,
+        categoryLevel: 3
       }
     };
   },
@@ -143,33 +164,93 @@ export default {
       }
     },
     //点击添加按钮显示的添加的页面
-    showAddDiv(){
-      this.isShoeList = false
+    showAddDiv() {
+      this.isShoeList = false;
       this.attrForm = {
-        attrName:'',
-        attrValueList:[],
-        categoryI:this.categary3Id,
-        categoryLevel:3,
-      }
+        attrName: "",
+        attrValueList: [],
+        categoryI: this.categary3Id,
+        categoryLevel: 3
+      };
     },
     //点击添加属性值
-    addAttrValue(){
+    addAttrValue() {
       this.attrForm.attrValueList.push({
-        attrId:this.attrForm.id,
+        attrId: this.attrForm.id,
         // id:0,   新添加的属性值没有id
-        valueName:"",
-        isEdit:true,//编辑模式
-      })
+        valueName: "",
+        isEdit: true //编辑模式
+      });
+      this.$nextTick(() => {
+        this.$refs[this.attrForm.attrValueList.length - 1].focus();
+      });
     },
-    //点击修改属性按钮   
-    shoeUpdataDiv(row){
-      this.isShoeList = false
-      this.attrForm = cloneDeep(row) //浅拷贝搞不定了   里面有一个数组
+    //点击修改属性按钮
+    shoeUpdataDiv(row) {
+      this.isShoeList = false;
+      this.attrForm = cloneDeep(row); //浅拷贝搞不定了   里面有一个数组
       //修改属性值当中已经存在了  属性  双击可编辑
-   //   this.attrForm.attrValueList.forEach(item => item.isEdit = false)    不是响应式属性
-      //修改属性值当中已经存在了  属性  双击可编辑
-     this.attrForm.attrValueList.forEach(item => this.$set(item,"isEdit",false))    
+      //   this.attrForm.attrValueList.forEach(item => item.isEdit = false)    不是响应式属性
+      this.attrForm.attrValueList.forEach(item =>
+        this.$set(item, "isEdit", false)
+      );
     },
+    //输入框回车事件
+    toLook(row) {
+      //失去焦点判定是否为空或重复
+      // console.log(row);
+      // console.log(this.attrForm.attrValueList);
+
+      if (row.valueName.trim() === "") {
+        this.$message.warning("请输入属性值");
+        row.valueName = "";
+        return;
+      }
+      let isRepeat = this.attrForm.attrValueList.some(item => {
+        // console.log(item);
+        if (item !== row) {
+          //
+          return item.valueName === row.valueName;
+        }
+      });
+      if (isRepeat) {
+        this.$message.warning("重复");
+        row.valueName = "";
+        return;
+      }
+      row.isEdit = false;
+    },
+    //span点击变input事件
+    toEdit(row, index) {
+      row.isEdit = true;
+      //变为input获取聚焦点
+      this.$nextTick(() => {
+        this.$refs[index].focus();
+      });
+    },
+    //保存操作
+   async save() {
+      //获取参数,整理参数,发送请求,成功... , 失败....
+      let attr = this.attrForm;
+      attr.attrValueList = attr.attrValueList.filter(item => {
+        if (item.valueName !== "") {
+          delete item.isEdit;
+          return true;
+        }
+      });
+      if(  attr.attrValueList.length === 0){
+        return
+      }
+      try {
+
+      await  this.$API.attr.addAttrValue(attr)
+      this.$message.success('成功')
+      this.isShoeList = true
+      this.getAttrList()
+      } catch (error) {
+        this.$message.error('保存失败')
+      }
+    }
   }
 };
 </script>
